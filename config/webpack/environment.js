2b561799803f0aa93ccc146d3a11a987444c7fd8
peresvetjke <igor.udalov@gmail.com>
2022-01-31 20:12:09 +0300

    Install vue js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 const path = require('path');
 
@@ -25,4 +27,6 @@ environment.loaders.prepend('Handlebars', {
   }
 });
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
